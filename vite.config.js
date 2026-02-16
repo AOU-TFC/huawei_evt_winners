756f385ffcf512c34733ea728e36a593e3f50057
Abdallah <abdallah.s_ah@outlook.com>
2026-02-16 21:31:24 +0100

    logo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-raffle-game/", // <--- ADD THIS LINE
+  base: "/huawei_evt_winners/", // <--- ADD THIS LINE
 });
